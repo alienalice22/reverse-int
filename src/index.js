6d897ea9b2a0eea141219d4ff367387a4af074b4
alienalice22 <alicehegay2202@gmail.com>
2023-07-07 19:58:55 +0500

    init: function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    let s = String(Math.abs(n));
+    for (let i = s.length; i > 0; i--) {
+        result += s[i - 1];
+        console.log(result);
+    }
+    return +result;
 }
